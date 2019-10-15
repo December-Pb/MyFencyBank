b0d69eaf1fe691d8332a35dcacbf055a5a4dbacf
sunjiaqian1218@163.com <sunjiaqian1218@163.com>
2019-10-15 14:12:37 -0400

    Revert "Add GUI, controller and model"
    This reverts commit af160d9

--- src/User/BankMananger.java
+++
@@ -1,9 +0,0 @@
-package User;
-
-public class BankMananger extends BankSystemUser {
-
-    public BankMananger(String ID, String userName, String password) {
-        super(ID, userName, password);
-    }
-
-}
